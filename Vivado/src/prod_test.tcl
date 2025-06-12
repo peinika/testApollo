f5b2c497290cbadff6c1e2c34faa7f6edfff56d9
Wren <rlo42@cornell.edu>
2025-06-12 11:37:49 -0400

    Update prod_test.tcl

--- Vivado/src/prod_test.tcl
+++ Vivado/src/prod_test.tcl
@@ -194,6 +194,7 @@ set files [list \
  [file normalize "${origin_dir}/src/reg_map.sv"] \
  [file normalize "${origin_dir}/src/i2c_slave.vhd"] \
  [file normalize "${origin_dir}/src/toplevel.v"] \
+ [file normalize "${origin_dir}/src/blinky.v"] \
 ]
 add_files -norecurse -fileset $obj $files
 
@@ -219,6 +220,11 @@ set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "VHDL" -objects $file_obj
 
 set file "$origin_dir/src/toplevel.v"
+set file [file normalize $file]
+set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
+set_property -name "file_type" -value "SystemVerilog" -objects $file_obj
+
+set file "$origin_dir/src/blinky.v"
 set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "SystemVerilog" -objects $file_obj
