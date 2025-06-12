0df0fc8d7f84012435dcedc76487afd4a14c8511
Wren <rlo42@cornell.edu>
2025-06-12 11:35:03 -0400

    Add files via upload

---
+++ Vivado/src/blinky.v
@@ -0,0 +1,29 @@
+module blinky (
+    input wire i_clock,
+    input wire i_enable,
+    output wire o_led_drive
+);
+
+    // Constants
+    // Equation: (clk freq (Hz) / blink freq (Hz) * 50% duty cycle) e.g. 100,000,000/2 *0.5
+
+    localparam integer C_CNT_05HZ = 25000000;
+
+    // Registers
+    reg [23:0] r_CNT_05HZ = 0;  // 24 bits is enough for counting up to 10 million
+    reg r_TOGGLE_05HZ = 0;
+
+    // Counter and toggle logic
+    always @(posedge i_clock) begin
+        if (r_CNT_05HZ == C_CNT_05HZ - 1) begin
+            r_CNT_05HZ <= 0;
+            r_TOGGLE_05HZ <= ~r_TOGGLE_05HZ;
+        end else begin
+            r_CNT_05HZ <= r_CNT_05HZ + 1;
+        end
+    end
+
+    // Output logic
+    assign o_led_drive = (i_enable == 1'b1) ? r_TOGGLE_05HZ : 1'b0;
+
+endmodule
