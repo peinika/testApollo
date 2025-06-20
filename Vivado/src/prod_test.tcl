e8ddf9832090c0631d4fb26d205490650cc1bf40
Wren <rlo42@cornell.edu>
2025-06-20 14:13:35 -0400

    Update prod_test.tcl

--- Vivado/src/prod_test.tcl
+++ Vivado/src/prod_test.tcl
@@ -221,6 +221,11 @@ set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "SystemVerilog" -objects $file_obj
 
+set file "$origin_dir/src/blinky.v"
+set file [file normalize $file]
+set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
+set_property -name "file_type" -value "SystemVerilog" -objects $file_obj
+
 
 # Set 'sources_1' fileset file properties for local files
 # None
@@ -229,12 +234,6 @@ set_property -name "file_type" -value "SystemVerilog" -objects $file_obj
 set obj [get_filesets sources_1]
 set_property -name "dataflow_viewer_settings" -value "min_width=16" -objects $obj
 set_property -name "top" -value "top" -objects $obj
-
-set file "$origin_dir/src/blinky.v"
-set file [file normalize $file]
-set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
-set_property -name "file_type" -value "SystemVerilog" -objects $file_obj
-
 
 # Set 'sources_1' fileset object
 set obj [get_filesets sources_1]
